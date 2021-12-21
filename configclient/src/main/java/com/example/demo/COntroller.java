2c3c00635b0323012a6c46959a22448f3d19642b
govind <84625932+hexortree@users.noreply.github.com>
2021-12-21 17:31:34 +0530

    b

---
+++ configclient/src/main/java/com/example/demo/COntroller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class COntroller {
+
+
+		@Value("${name}")
+		private String name;
+		
+		@GetMapping("/")
+		public String getName() {
+			return name;
+		}
+	}
+
